cd5295de21cec5e29a4b481c6752627a854af109
Anton <ant.ministr@yandex.ru>
2014-05-28 01:24:54 +0400

    finish

--- src/Square.java
+++ src/Square.java
@@ -1,9 +1,9 @@
 public class Square {
-    String x;
-    String y;
-    String size;
-    String color;
-    String number;
+    private String x;
+    private String y;
+    private String size;
+    private String color;
+    private String number;
 
     public Square(String x, String y, String size, String color, String number) {
         this.x = x;
@@ -11,5 +11,46 @@ public class Square {
         this.size = size;
         this.color = color;
         this.number = number;
+    }
+
+    public String getX() {
+        return x;
+    }
+
+    public String getY() {
+        return y;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setX(String x) {
+        this.x = x;
+    }
+
+    public void setY(String y) {
+        this.y = y;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String toString() {
+        return "<div style=\"position: absolute; " + "width: " + size + "px;" + "height: " + size + "px;" +
+                "background-color: " + color + ";" + "left: " + x + "px;" + "top: " + y + "px; \">" + number + "</div>\n";
     }
 }
